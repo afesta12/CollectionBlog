c69108c88d4b88e6fd4f094db93d87a1fcf3d428
andrewfesta1 <73345981+andrewfesta1@users.noreply.github.com>
2022-09-08 21:30:47 -0400

    Form updates

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,38 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
+import FileBase from 'react-file-base64';
 
 const Form = () => {
+ 
+    const [postData, setPostData] = useState({
+        user: '',
+        title: '',
+        message: '',
+        tags: '',
+        selectedFile: ''
+
+    })
+    const handleSubmit = () => {
+        
+    }
+
+    const clear = () => {
+        
+    }
+
   return (
     
         <div className="container absolute">
         
-            <form action="" className="form">
+            <form autoComplete='off' noValidate className="form" onSubmit={handleSubmit}>
             
                 <div className="form__container">
                 
-                    <input type="text" className="form__input" placeholder='User' />
-                    <input type="text" className="form__input" placeholder='Post Title' />
+                    <input type="text" 
+                           className="form__input" 
+                           placeholder='User'
+                           name='user'
+                           value={postData.user}
+                           onChange={(e) => setPostData({...postData, user: e.target.value})} />
+
+                    <input type="text" 
+                           className="form__input" 
+                           placeholder='Post Title'
+                           name='title'
+                           value={postData.title}
+                           onChange={(e) => setPostData({...postData, title: e.target.value})} />
 
                 </div>
 
                 <div className="form__container">
                 
-                    <textarea className='form__textarea' name="" id="" cols="25" rows="5" placeholder='Post Message'></textarea>
+                    <textarea className='form__textarea' 
+                              name="message" 
+                              id="" 
+                              cols="25" 
+                              rows="5" 
+                              placeholder='Post Message'
+                              value={postData.message}
+                              onChange={(e) => setPostData({...postData, message: e.target.value})}>
+                    </textarea>
 
                 </div>
 
                 <div className="form__container">
                 
-                    <input type="text" className="form__input" placeholder='Tags' />
+                    <input type="text" 
+                           className="form__input" 
+                           placeholder='Tags'
+                           value={postData.tags}
+                           name='tags'
+                           onChange={(e) => setPostData({...postData, tags: e.target.value})} />
                 
                 </div>
 
                 <div className="form__container">
                 
-                    <input type="file" className="form__input" />
+                    <FileBase 
+                        type='file'
+                        multiple={false}
+                        onDone={(base64) => setPostData({...postData, selectedFile: base64})}
+                    />
                 
                 </div>
                 
-                <input type="submit" className='submit'/>
+                <div className="form__flex">
+
+                    <input type="submit" className='submit'/>
+                    <button className="submit" onClick={clear} >Clear</button>
+                
+                </div>
+                
             </form>
         
         </div>
